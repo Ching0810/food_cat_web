e70b961340f0f3fa6fbb9c6ca36a3ef87600a325
Ching0810 <neinchin123@gmail.com>
2023-08-04 14:52:58 +0800

    feat: add listing page

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+const restaurants = require('./public/jsons/restaurant.json').results
+
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
 
 // express.static is express build-in middleware function
 // after app.use(express.static('public')), server could access to file inside public direction
@@ -11,7 +17,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurants', (req, res) => {
-  res.send('redirect to /restaurants and listing options')
+  res.render('index', { restaurants })
 })
 
 // :id的意思就是產生一個key為id、value為輸入變數的pair存在object裡，結果為{ id: 輸入變數值 }，所以可以用來設計動態路由
