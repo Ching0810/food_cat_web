fdd9f7f6a5f7d848f13db8ee51a81ead749bae4b
Ching0810 <neinchin123@gmail.com>
2023-08-04 13:52:58 +0800

    add note: route params

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.get('/restaurants', (req, res) => {
   res.send('redirect to /restaurants and listing options')
 })
 
+// :id的意思就是產生一個key為id、value為輸入變數的pair存在object裡，結果為{ id: 輸入變數值 }，所以可以用來設計動態路由
 app.get('/restaurant/:id', (req, res) => {
   const id = req.params.id
   res.send(`read restaurant: ${id}`)
