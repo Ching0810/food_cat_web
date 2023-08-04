9f58de6fd49e4bc1fbdae84e5e6dd687bdb9ad9c
Ching0810 <neinchin123@gmail.com>
2023-08-05 00:56:42 +0800

    feat: add search funtion to listing (restaurant name result only) and note

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ app.get('/', (req, res) => {
 
 // { restaurants }: 用{ }把變數包起來，就可以在前端引入
 app.get('/restaurants', (req, res) => {
-  res.render('index', { restaurants })
+  // keyword = req.query."search"要符合search bar中input的name，且會直接等於路由?之後的參數
+  const keyword = req.query.search
+  const matchedRestaurants = keyword? restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase())): restaurants
+  // { restaurants: matchedRestaurants }就是object的key-value pair的意思，前端網頁可以引入key: restaurants把matched的餐廳帶入，所以key值restaurants跟變數restaurants無關，要改成什麼都可以，只要在前端引入正確的key值即可順利帶入
+  res.render('index', { restaurants: matchedRestaurants, keyword })
 })
 
 // :id的意思就是產生一個key為id、value為輸入變數的pair存在object裡，結果為{ id: 輸入變數值 }，所以可以用來設計動態路由
